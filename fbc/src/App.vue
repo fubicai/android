6157931eb1fc551215c47ae98916c8b62ca975bf
cancerts <cxsblock@gmail.com>
2019-01-25 10:01:15 +0800

    the h5 struc

---
+++ fbc/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <Header />
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Footer />
+  </div>
+</template>
+
+<script>
+import "bulma";
+import "../node_modules/mint-ui/lib/style.css";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+
+export default {
+  name: 'app',
+  components: {
+    Header,
+    Footer
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
